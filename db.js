dead8733e0e1f7304c3c72f9779760a9a4ef32df
unknown <kvaibhav467@gmail.com>
2024-06-06 00:24:58 +0530

    version 1.2

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 
 // Define the MongoDB connection URL
-const mongoURL = "mongodb://127.0.0.1:27017/hotels"
+// const mongoURL = "mongodb://127.0.0.1:27017/hotels"
+// const mongoURL = "mongodb+srv://kvaibhav467:vaibhav123@cluster0.lajylnu.mongodb.net/"
+const mongoURL = process.env.MONGODB_URL
 
 
 // Set up MongoDB connection
-mongoose.connect(mongoURL)
+mongoose.connect(mongoURL);
 
 
 // Get the default connection
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require("dotenv").config();
+
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000
 
 
 app.get("/", function (req,res) {
   res.send("Welcome to my hotel... How i can help you?, we have list of menus")
 })
-
-
 
 
 
@@ -23,6 +25,6 @@ app.use("/menu",menuRoutes);
 
 
 
-app.listen(3000,()=>{
-  console.log("listening on port 3000");
+app.listen(PORT,()=>{
+  console.log(`listening on port ${PORT}`);
 })
